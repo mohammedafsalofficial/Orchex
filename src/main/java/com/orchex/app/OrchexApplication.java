b0c65f9988f81a1b709232e39989baf10cfb5723
Mohammed Afsal <mohammedafsal.a@codingmart.com>
2026-03-05 18:05:02 +0530

    feat: initial commit

---
+++ src/main/java/com/orchex/app/OrchexApplication.java
@@ -0,0 +1,12 @@
+package com.orchex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/orchex/app/OrchexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchex.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
