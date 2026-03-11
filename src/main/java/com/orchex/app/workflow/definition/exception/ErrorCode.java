08f10062e1efb187a98fe60faaefc6c447b8a125
Mohammed Afsal <mohammedafsaltut@gmail.com>
2026-03-11 09:59:25 +0530

    feat: GlobalExceptionHandler to handle exceptions

---
+++ src/main/java/com/orchex/app/workflow/definition/exception/ErrorCode.java
@@ -0,0 +1,6 @@
+package com.orchex.app.workflow.definition.exception;
+
+public enum ErrorCode {
+
+    WORKFLOW_ALREADY_EXISTS
+}
--- src/main/java/com/orchex/app/workflow/definition/repository/WorkflowDefinitionRepository.java
+++ src/main/java/com/orchex/app/workflow/definition/repository/WorkflowDefinitionRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface WorkflowDefinitionRepository extends JpaRepository<WorkflowDefinition, UUID> {
+
+    boolean existsByName(String name);
 }
